bbbb4bd3942d1af5c67cd43218602bafacdc1214
Trường Nguyễn <truongnguyen210199@gmail.com>
2022-02-26 13:41:47 +0700

    Update MainActivity.java

--- app/src/main/java/topica/edu/vn/bai08/MainActivity.java
+++ app/src/main/java/topica/edu/vn/bai08/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     int n=8;
+    String t="truong nguyen"
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
